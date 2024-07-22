eb966f386232ab700e35c9681ded83c0591dae88
handred800 <handred800@gmail.com>
2024-07-22 11:23:30 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <nuxt-link to="/static">static</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/static_time">static_time</nuxt-link>
+            </li>
+        </ul>
+    </div>
+  </template>
+  
---
+++ pages/static_time.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>static_time</h1>
+    <h2>build: {{ new Date().toLocaleString() }}</h2>
+    <h2>{{ time }}</h2>
+  </div>
+</template>
+<script setup>
+const time = await $fetch("/api/now").then((res) => {
+  console.log(res.timestamp);
+  return new Date(res.timestamp).toLocaleString();
+});
+</script>
